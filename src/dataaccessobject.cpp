5e0f7c73c97652b672d82190ee1aa1891d6c0654
Niklas Wulf <niklas@wulf-home.de>
2014-02-13 15:34:31 +0100

    Fixed: Bug where readAll did not return all objects, when the table has been empty before.

--- src/dataaccessobject.cpp
+++ src/dataaccessobject.cpp
@@ -174,6 +174,8 @@ QSharedPointer<QObject> QpDaoBase::createObject()
     }
     QSharedPointer<QObject> obj = data->cache.insert(Qp::Private::primaryKey(object), object);
     Qp::Private::enableSharedFromThis(obj);
+    count();
+
     ++data->count;
 
     emit objectCreated(obj);
@@ -205,6 +207,7 @@ bool QpDaoBase::removeObject(QSharedPointer<QObject> object)
         setLastError(data->sqlDataAccessObjectHelper->lastError());
         return false;
     }
+    count();
 
     --data->count;
     data->cache.remove(Qp::primaryKey(object));
