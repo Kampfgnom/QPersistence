4eed268c78580d97106567ad22b69314630fc257
Niklas Wulf <niklas.wulf@geenen-it-systeme.de>
2014-04-14 16:49:32 +0200

    Angebote weiter verbessert.

--- src/qpersistence_impl.cpp
+++ src/qpersistence_impl.cpp
@@ -104,6 +104,7 @@ UpdateResult update(QSharedPointer<T> object)
     beginTransaction();
     Qp::UpdateResult result = QpDaoBase::forClass(*object->metaObject())->updateObject(object);
     if(result == Qp::UpdateConflict) {
+        qWarning() << "Update conflict for " << T::staticMetaObject.className() << Qp::primaryKey(object);
         Qp::database().rollback();
         return Qp::UpdateConflict;
     }
